5e67cfd2e5bcb1da814cd624dd9819467661aca6
Mehmet Fatih Karakuş <fatihkarakus563@gmail.com>
2024-09-18 12:09:41 +0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { auth } from '@clerk/nextjs/server'
 
 // /admin, /preview, /create rotalarını koruyalım, / kök dizinini hariç tutalım
 const isProtectedRoute = createRouteMatcher(["/admin", "/preview", "/create", "/payment"]);
 const isRootPath = createRouteMatcher(["/"]);
-export default clerkMiddleware((auth, req) => {
 
-  const {userId} = auth();
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
 
   if (isProtectedRoute(req)) {
     auth().protect();  // Korunan rotalara girilmek istendiğinde auth() tetiklenir
   }
 
-  if(isRootPath(req) && userId){
+  if (isRootPath(req) && userId) {
     return NextResponse.redirect(new URL("/admin", req.url));
   }
+
   return NextResponse.next();
 });
 
-
-
-
+// Bu ayar middleware'in Edge Runtime yerine Node.js ortamında çalışmasını sağlar
 export const config = {
+  runtime: 'nodejs',  // Node.js ortamında çalışmasını sağlıyoruz
   matcher: [
     // Next.js internal dosyaları ve statik dosyaları atla
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
