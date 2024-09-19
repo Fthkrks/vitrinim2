1563bdd71b0d786cb23832c2fd0ad8713b752d78
Mehmet Fatih Karakuş <fatihkarakus563@gmail.com>
2024-09-19 11:20:11 +0300

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,7 +35,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="tr">
         <body className={inter.className}>
           <div data-theme="dark" className=" h-screen">
