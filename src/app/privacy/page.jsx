b67cef72e2d7a2ca906ecce11b8ccb26fe37ae69
Mehmet Fatih Karakuş <fatihkarakus563@gmail.com>
2024-09-19 16:04:22 +0300

    privacy added

---
+++ src/app/privacy/page.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+
+function Privacy() {
+  return (
+    <div className="flex items-center justify-center p-20">
+      <div>
+        <h1 className="text-3xl font-bold">Gizlilik Politikası</h1>
+
+        <p>
+          Fatih Karakuş olarak (veya "Biz"), kullanıcılarımızın gizliliğine
+          saygı duyuyoruz ve gizliliklerini korumak için çalışıyoruz. Bu
+          Gizlilik Politikası, web sitemizi ziyaret ettiğinizde ve
+          hizmetlerimizi kullandığınızda hangi bilgileri topladığımızı, bu
+          bilgileri nasıl kullandığımızı ve koruduğumuzu açıklamaktadır.
+        </p>
+
+        <h2>1. Toplanan Bilgiler</h2>
+
+        <p>
+          Sitemizi ziyaret ettiğinizde ve hizmetlerimizi kullandığınızda,
+          aşağıdaki kişisel bilgileri toplayabiliriz:
+        </p>
+
+        <ul>
+          <li>Ad, soyad, e-posta adresi gibi kişisel bilgiler.</li>
+          <li>
+            IP adresiniz, tarayıcı türünüz ve ziyaret ettiğiniz sayfalar gibi
+            otomatik olarak toplanan bilgiler.
+          </li>
+          <li>
+            Çerezler ve benzeri izleme teknolojileri ile toplanan bilgiler.
+          </li>
+        </ul>
+
+        <h2>2. Bilgilerin Kullanımı</h2>
+
+        <p>Toplanan kişisel bilgileri aşağıdaki amaçlarla kullanabiliriz:</p>
+
+        <ul>
+          <li>Hizmetlerimizi sunmak ve geliştirmek.</li>
+          <li>Kullanıcı deneyimini iyileştirmek.</li>
+          <li>Sorularınızı ve taleplerinizi yanıtlamak.</li>
+          <li>Yasal gerekliliklere uyum sağlamak.</li>
+        </ul>
+
+        <h2>3. Bilgilerin Paylaşımı</h2>
+
+        <p>
+          Kişisel bilgilerinizi üçüncü taraflarla paylaşmayız, ancak aşağıdaki
+          durumlarda paylaşabiliriz:
+        </p>
+
+        <ul>
+          <li>Yasal yükümlülüklere uymak amacıyla.</li>
+          <li>
+            Hizmet sağlayıcılarla hizmetleri sunabilmek için (örneğin,
+            barındırma sağlayıcıları, analiz hizmetleri).
+          </li>
+        </ul>
+
+        <h2>4. Çerezler</h2>
+
+        <p>
+          Web sitemizde çerezler kullanılabilir. Çerezler, web sitemizi ziyaret
+          ettiğinizde tarayıcınıza gönderilen küçük veri dosyalarıdır. Çerezler,
+          web sitemizin kullanımını analiz etmemize ve deneyiminizi
+          kişiselleştirmemize yardımcı olur. Tarayıcınızın ayarlarını kullanarak
+          çerezleri devre dışı bırakabilirsiniz, ancak bu durumda web sitemizin
+          bazı özellikleri düzgün çalışmayabilir.
+        </p>
+
+        <h2>5. Üçüncü Taraf Bağlantılar</h2>
+
+        <p>
+          Web sitemiz, üçüncü taraf web sitelerine bağlantılar içerebilir. Bu
+          sitelerin gizlilik uygulamaları bizim kontrolümüz dışında olduğundan,
+          bu üçüncü taraf sitelerin gizlilik politikalarını incelemenizi
+          öneririz.
+        </p>
+
+        <h2>6. Güvenlik</h2>
+
+        <p>
+          Kişisel bilgilerinizin güvenliğini sağlamak için uygun önlemler
+          alıyoruz. Ancak, internet üzerinden iletilen verilerin tamamen güvenli
+          olduğunu garanti edemeyiz.
+        </p>
+
+        <h2>7. Haklarınız</h2>
+
+        <p>
+          Kişisel verilerinize erişme, bunları düzeltme veya silme hakkına
+          sahipsiniz. Bu haklarınızı kullanmak için bizimle iletişime
+          geçebilirsiniz.
+        </p>
+
+        <h2>8. İletişim</h2>
+
+        <p>
+          Gizlilik politikamız hakkında sorularınız veya endişeleriniz varsa,{" "}
+          <a href="mailto:youremail@example.com">youremail@example.com</a>{" "}
+          üzerinden bizimle iletişime geçebilirsiniz.
+        </p>
+
+        <h2>9. Değişiklikler</h2>
+
+        <p>
+          Bu Gizlilik Politikası, zaman zaman güncellenebilir. Değişiklikler,
+          web sitemizde yayınlandıktan sonra yürürlüğe girer.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Privacy;
